97290cfc1300b4beaf9458b3baf268bf21815aa5
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2025-05-20 09:30:31 +0100

    Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.0.0"
-  private val hmrcMongoVersion = "2.0.0"
+  private val bootstrapVersion = "9.12.0"
+  private val hmrcMongoVersion = "2.6.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"      % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"             % hmrcMongoVersion,
-    "uk.gov.hmrc"             %% "internal-auth-client-play-30"   % "3.0.0"
+    "uk.gov.hmrc"             %% "internal-auth-client-play-30"   % "4.0.0"
   )
 
   val test = Seq(
